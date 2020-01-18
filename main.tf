6740ac10be1819e1bc2429a789799b4c3e7a43b2
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2020-01-18 13:54:15 +0530

    Add proper formatting to files

--- main.tf
+++ main.tf
@@ -28,15 +28,15 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy" {
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
-  version = var.eks_version == "" ? null : var.eks_version
+  version  = var.eks_version == "" ? null : var.eks_version
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids              = var.subnet_ids
     endpoint_private_access = var.enable_private_access
-    endpoint_public_access = var.enable_public_access
-    public_access_cidrs = var.enable_public_access == true ? var.public_cidrs : null
+    endpoint_public_access  = var.enable_public_access
+    public_access_cidrs     = var.enable_public_access == true ? var.public_cidrs : null
   }
 
   enabled_cluster_log_types = var.eks_log_types
--- vars.tf
+++ vars.tf
@@ -1,22 +1,29 @@
 variable "vpc_id" {}
+
 variable "cluster_name" {}
+
 variable "subnet_ids" {}
+
 variable "eks_version" {
-    default = ""
+  default = ""
 }
+
 variable "enable_private_access" {
   default = true
 }
+
 variable "enable_public_access" {
   default = false
 }
+
 variable "public_cidrs" {
   type = list
   default = [
-      "0.0.0.0/0"
+    "0.0.0.0/0"
   ]
 }
+
 variable "eks_log_types" {
-  type = list
+  type    = list
   default = []
 }
