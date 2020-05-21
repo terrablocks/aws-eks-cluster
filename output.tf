7264dc7d1a8af758595f89dfd6ddbfebeaf49d40
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2020-05-21 16:15:34 +0530

    Update output value

--- output.tf
+++ output.tf
@@ -15,7 +15,7 @@ output "ca_data" {
 }
 
 output "oidc_url" {
-  value = aws_eks_cluster.eks_cluster.identity.issuer
+  value = aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer
 }
 
 output "sg_id" {
